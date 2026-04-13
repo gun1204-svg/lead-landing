f54aac3eb26fef7803c7848e6d2e64b2b9cd5962
Gun <yourgithub@email.com>
2026-04-14 05:22:46 +0900

    fix: replace tiktok pixel loader

--- components/AnalyticsScripts.tsx
+++ components/AnalyticsScripts.tsx
@@ -32,6 +32,9 @@ declare global {
     _nasa?: Record<string, unknown>;
     ttq?: {
       track: (eventName: string, params?: Record<string, unknown>) => void;
+      page?: () => void;
+      load?: (pixelId: string, options?: Record<string, unknown>) => void;
+      identify?: (params?: Record<string, unknown>) => void;
     };
   }
 }
@@ -98,7 +101,7 @@ export function trackLeadComplete(params?: {
     console.error("Naver Lead track error:", e);
   }
 
-  // ✅ TikTok (🔥 핵심)
+  // ✅ TikTok
   try {
     if (window.ttq) {
       window.ttq.track("Lead", safeParams);
@@ -166,17 +169,10 @@ fbq('track', 'PageView');
         dangerouslySetInnerHTML={{
           __html: `
 !function (w, d, t) {
-  w.TiktokAnalyticsObject=t;
-  var ttq=w[t]=w[t]||[];
-  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group"];
-  ttq.setAndDefer=function(t,e){
-    t[e]=function(){
-      t.push([e].concat(Array.prototype.slice.call(arguments,0)));
-    };
-  };
-  for(var i=0;i<ttq.methods.length;i++){
-    ttq.setAndDefer(ttq,ttq.methods[i]);
-  }
+  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");
+n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
   ttq.load('${TIKTOK_PIXEL_ID}');
   ttq.page();
 }(window, document, 'ttq');
